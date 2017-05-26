7177cee708b58d093ce4477b9bd70d5c4c8d6b67
limelime <limelime@users.noreply.github.com>
2017-05-26 14:55:41 -0400

    Use file as input.

--- ih-create-ics.sh
+++ ih-create-ics.sh
@@ -1,17 +1,21 @@
-holiday=$1
-ics_file=ical-holiday.ics
+event_date=$1
+event_title=$2
 
+ics_file=ical-event_date.ics
+
+sequence=$(date +%s)
 cat >> ${ics_file} <<EOF
 BEGIN:VCALENDAR
 VERSION:2.0
 PRODID:-//Manual//NONSGML My Product//EN
 BEGIN:VEVENT
-UID:uid1@example.com
+UID:${event_date}
+SEQUENCE:${sequence}
 DTSTAMP:19970714T170000Z
 ORGANIZER;CN=John Doe:MAILTO:john.doe@example.com
-DTSTART;TZID=America/New_York:${holiday}T080000
-DTEND;TZID=America/New_York:${holiday}T170000
-SUMMARY: Public Holiday - Montreal
+DTSTART;TZID=America/New_York:${event_date}T080000
+DTEND;TZID=America/New_York:${event_date}T170000
+SUMMARY:${event_title}
 END:VEVENT
 END:VCALENDAR
 EOF
--- ih.sh
+++ ih.sh
@@ -1,10 +1,22 @@
-ics_file=ical-holiday.ics
-> ${ics_file}
+input_file=$1
 
-for (( i=1; i<=$#; i++)); do
+# Output file.
+  ics_file=ical-event_date.ics
+  > ${ics_file}
 
-./ih-create-ics.sh "${!i}"
+# Create icalendar file.
+  while IFS='' read -r file_line || [[ -n "${file_line}" ]]; do
+
+    if echo "${file_line}" | grep -q ';' ; then
+      event_date=$(echo "${file_line}" | cut -d';' -f1)
+      event_title=$(echo "${file_line}" | cut -d';' -f2)    
+    else
+      event_date=${file_line}
+      event_title=''
+    fi
     
-done
+    ./ih-create-ics.sh "${event_date}" "${event_title}"
+   
+  done < <( cat ${input_file} | grep -v "^#"  | awk NF) # Ignore comment line | Remove empty line
 
 echo "All holidays created in ${ics_file}."
